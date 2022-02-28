355a102cc11d7b2ce3a06731106db221f536f24b
Paul Bernhardt <pbernhardt@gmail.com>
2022-02-27 21:14:31 -0500

    fetching token

--- source/spotify.test.ts
+++ source/spotify.test.ts
@@ -2,6 +2,9 @@ import { Spotify } from './spotify';
 
 describe('when inializing the spotify api', () => {
 	it('should create a spotify client', () => {
-		const client = new Spotify('x', 'y');
+		const spotify = new Spotify('x', 'y', 'z');
+		expect(spotify.client.getClientId()).toBe('x');
+		expect(spotify.client.getClientSecret()).toBe('y');
+		//expect(spotify.client.getAccessToken()).toBe('z');
 	});
 });
--- source/spotify.ts
+++ source/spotify.ts
@@ -1,5 +1,21 @@
+import SpotifyWebApi from 'spotify-web-api-node';
+
+const redirectUri = 'https://example.com/callback';
 export class Spotify {
-	constructor(clientId: string, secret: string) {
-		console.log(clientId, secret);
+	client: SpotifyWebApi;
+	constructor(clientId: string, clientSecret: string, code: string) {
+		console.log(clientId, clientSecret);
+		this.client = new SpotifyWebApi({
+			clientId,
+			clientSecret,
+			redirectUri,
+		});
+	}
+
+	async fetchToken(code) {
+		const result = await this.client.authorizationCodeGrant(code);
+		this.client.setAccessToken(result.body.access_token);
+		this.client.setRefreshToken(result.body.refresh_token);
+		//this.client.refreshAccessToken()
 	}
 }
